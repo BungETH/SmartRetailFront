d7d445a81bcf2a73aec5cccb9cbc89cdb0be2a23
damienh972 <helo.dams@gmail.com>
2020-12-17 12:41:22 +0100

    add material-ui

--- client/src/components/Dapp/index.js
+++ client/src/components/Dapp/index.js
@@ -1,19 +1,24 @@
+import PropTypes from 'prop-types';
 import React from 'react';
 
-import "./dapp.scss";
+import './dapp.scss';
 
 const Dapp = ({ drizzle, drizzleState, account }) => {
 
-	// console.log(drizzle);
-	// console.log(drizzleState);
-	// console.log(account);
+  // console.log(drizzle);
+  // console.log(drizzleState);
+  // console.log(account);
 
-	return(
-		<div className='dapp'>
-			
-		</div>
-	)
-}	 
+  return (
+    <div className="dapp">
+    </div>
+  );
+};
 
+Dapp.propTypes = {
+  account: PropTypes.any.isRequired,
+  drizzle: PropTypes.any.isRequired,
+  drizzleState: PropTypes.any.isRequired,
+};
 
 export default Dapp;
