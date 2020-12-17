7c7cf5dd451d926fdc59ea0da87b76bbfc3f714f
damienh972 <helo.dams@gmail.com>
2020-12-17 11:38:49 +0100

    drizzle init

---
+++ client/src/components/Dapp/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import "./dapp.scss";
+
+const Dapp = ({ drizzle, drizzleState, account }) => {
+
+	// console.log(drizzle);
+	// console.log(drizzleState);
+	// console.log(account);
+
+	return(
+		<div className='dapp'>
+			
+		</div>
+	)
+}	 
+
+
+export default Dapp;
