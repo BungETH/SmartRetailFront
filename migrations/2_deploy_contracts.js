6be45e905acb1d734b4514ca216ab975f401f82f
damienh972 <helo.dams@gmail.com>
2020-12-18 17:03:31 +0100

    some fix

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -5,5 +5,5 @@ const FidelityToken = artifacts.require("FidelityToken");
 module.exports = function(deployer) {
   deployer.deploy(SimpleStorage);
   deployer.deploy(ComplexStorage);
-  deployer.deploy(FidelityToken,0);
+  deployer.deploy(FidelityToken);
 };
