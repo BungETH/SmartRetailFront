c1a2c55e3457c23fd833c91275a3e6d52b6a28a9
damienh972 <helo.dams@gmail.com>
2020-12-19 15:33:39 +0100

    token amount minted show on interface

--- client/src/components/Dapp/Marketplace.js
+++ client/src/components/Dapp/Marketplace.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import axios from 'axios';
+import React, { useState }from 'react';
+// import axios from 'axios';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
@@ -12,11 +12,13 @@ import testImg from '../../assets/citation_NH_mindPower.jpg';
 
 const Marketplace = ({drizzle, account}) => {
   console.log(drizzle);
+  const [rewards,setRewards] = useState(0);
   const contract = drizzle.contracts.FidelityToken; 
   const price = 1000;
   const claimToken = async (price) => {
     const getToken = await contract.methods.claim(price*0.05).send({gas: 900000, from: account });
     console.log(getToken);
+    setRewards(getToken.events.Transfer.returnValues.value)
   }
   const useStyles = makeStyles({
     root: {
@@ -50,40 +52,46 @@ const Marketplace = ({drizzle, account}) => {
     claimToken(price);
   }
 
+  console.log(rewards);
   return (
-    <Card raised className={classes.root}>
-      <CardActionArea>
-        <CardMedia
-          className={classes.media}
-          image={testImg}
-          title="Contemplative Reptile"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-            Audio Book Reader
+    <div>
+      <Card raised className={classes.root}>
+        <CardActionArea>
+          <CardMedia
+            className={classes.media}
+            image={testImg}
+            title="Contemplative Reptile"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+              Audio Book Reader
+            </Typography>
+            <Typography variant="body2" color="textSecondary" component="p">
+              Some text about product details
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          <Button size="small" color="primary" onClick={handleBuy}>
+            Buy
+          </Button>
+          <Button size="small" color="primary">
+            Show details
+          </Button>
+          <Typography
+            className={classes.price}
+            variant="h4"
+            color="primary"
+            component="p"
+          >
+            {price} $
           </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-            Some text about product details
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-      <CardActions>
-        <Button size="small" color="primary" onClick={handleBuy}>
-          Buy
-        </Button>
-        <Button size="small" color="primary">
-          Show details
-        </Button>
-        <Typography
-          className={classes.price}
-          variant="h4"
-          color="primary"
-          component="p"
-        >
-          {price} $
-        </Typography>
-      </CardActions>
-    </Card>
+        </CardActions>
+      </Card>
+      <Typography gutterBottom variant="h5" component="h2">
+        {rewards}
+      </Typography>
+    </div>
   );
 };
 
