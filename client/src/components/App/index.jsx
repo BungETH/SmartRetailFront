321eb299ffee04768d7fcb3a624b98bf36dc47d0
Elie Vangu <vangu.elie@gmail.com>
2020-12-30 02:27:05 +0100

    loading component + promiseTracker on contract instanciation

--- client/src/components/App/index.jsx
+++ client/src/components/App/index.jsx
@@ -1,23 +1,24 @@
-import React, { useEffect, useState } from 'react';
-import { DrizzleContext } from '@drizzle/react-plugin';
-import { Drizzle } from '@drizzle/store';
-import store from '../../store';
-import drizzleOptions from '../../drizzleOptions';
-import Dapp from '../../containers/Dapp';
-import './app.scss';
+//npm import
+import React, { useEffect, useState } from "react";
+import { DrizzleContext } from "@drizzle/react-plugin";
+import { Drizzle } from "@drizzle/store";
+import Loading from "../ReactLoading/ReactLoading";
+//local import
+import store from "../../store";
+import drizzleOptions from "../../drizzleOptions";
+import Dapp from "../../containers/Dapp";
+import "./app.scss";
 
 // It instanciate new drizzle object with our drizzleOptions
 
 const drizzle = new Drizzle(drizzleOptions, store);
 
 const App = ({ fetchCurrentAccount, currentAccount }) => {
-  
-  
   async function getAccount() {
     const accounts = await window.ethereum.enable();
     fetchCurrentAccount(accounts[0]);
   }
-  window.ethereum.on('accountsChanged', () => {
+  window.ethereum.on("accountsChanged", () => {
     getAccount();
   });
 
@@ -27,26 +28,21 @@ const App = ({ fetchCurrentAccount, currentAccount }) => {
     getAccount();
   }, [currentAccount]);
 
-  
   return (
     // Here is native drizzle components who helps to Dapp initialisation
     <DrizzleContext.Provider drizzle={drizzle}>
-        <DrizzleContext.Consumer>
-          {(drizzleContext) => {
-            const { drizzleState, initialized } = drizzleContext;
-            if (!initialized) {
-              return "Loading...";
-            }
-            
-            return (
-              <div className="app">
-                <Dapp
-                  drizzle={drizzle}
-                />
-              </div>
-            );
-          }}
-        </DrizzleContext.Consumer>
+      <DrizzleContext.Consumer>
+        {(drizzleContext) => {
+          const { drizzleState, initialized } = drizzleContext;
+          return !initialized ? (
+            <Loading type={"bubbles"} color={"#3F51B5"} />
+          ) : (
+            <div className="app">
+              <Dapp drizzle={drizzle} />
+            </div>
+          );
+        }}
+      </DrizzleContext.Consumer>
     </DrizzleContext.Provider>
   );
 };
