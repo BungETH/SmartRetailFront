65ba474372426cb5429b51fcc9c763a9113968eb
Jason Martoux <apps@martoux.com>
2021-01-06 10:16:02 +0100

    update to interface contracts

--- contracts/SmartRetailEscrow.sol
+++ contracts/SmartRetailEscrow.sol
@@ -8,22 +8,23 @@ import "@openzeppelin/contracts/payment/escrow/Escrow.sol";
 import "./FDLTTokenManager.sol";
 
 contract TokenManagerInterface {
-    function asyncDeposit(address dest, uint256 amount) public{}
-    function claim() public {}
+    function asyncDeposit(address dest, uint256 amount) external{}
+    function claim(address dest) external {}
 }
 
-
 contract SmartRetailEscrow is Ownable, ReentrancyGuard {
-    Escrow escrow;
+    Escrow private escrow;
     TokenManagerInterface tokenManagerContract;
     
     event FundSendToContract(string _contractMessage);
     event FundSendToSeller(string _SellerMessage);
+
     enum State { AWAITING_PAYMENT, AWAITING_DELIVERY }
-    State public currState = State.AWAITING_PAYMENT;
+    mapping(address => State) currState;
+
     address payable seller;
     address buyer;
-    
+
     constructor(address _tokenManagerContractAddress) ReentrancyGuard() public {
         escrow = new Escrow();
         tokenManagerContract = TokenManagerInterface(_tokenManagerContractAddress);
@@ -37,22 +38,22 @@ contract SmartRetailEscrow is Ownable, ReentrancyGuard {
      */
     function sendPayment(address payable _seller, uint _value) external payable onlyOwner{
         seller = _seller;
-        require(currState == State.AWAITING_PAYMENT, "Already paid");
+        require(currState[msg.sender] == State.AWAITING_PAYMENT, "Already paid");
         require((_value) == msg.value, "You're not sending the correct value");
         escrow.deposit{value: msg.value}(seller);
+        currState[msg.sender] = State.AWAITING_DELIVERY;
         emit FundSendToContract("Successfully deposit funds to contract");
-        currState = State.AWAITING_DELIVERY;
     }
         
     /**
      * Withdraw funds to seller
      */
     function confirmDelivery(uint _amount) external onlyOwner nonReentrant() {
-        require(currState == State.AWAITING_DELIVERY, "You cannot confirm until deposit first");
+        require(currState[msg.sender] == State.AWAITING_DELIVERY, "You cannot confirm until deposit first");
         escrow.withdraw(seller);
         tokenManagerContract.asyncDeposit(msg.sender, _amount);
+        currState[msg.sender] = State.AWAITING_PAYMENT;
         emit FundSendToSeller("Successfully transferred funds to seller");
-        currState = State.AWAITING_PAYMENT;
     }
     
     function setTokenManagerContractAddress(address _address) external onlyOwner {
