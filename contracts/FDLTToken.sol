3ebb95692194590b49758769e515e395c5ca00ad
Jason Martoux <apps@martoux.com>
2021-01-07 23:35:51 +0100

    update TokenManager for Event child

--- contracts/FDLTToken.sol
+++ contracts/FDLTToken.sol
@@ -1,12 +1,13 @@
 // contrats / FDLTToken.sol
 // SPDX-License-Identifier: MIT
 
-pragma solidity >=0.4.21 <0.7.0;
+pragma solidity 0.6.12;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract FDLTToken is ERC20 {
 
+<<<<<<< HEAD
 	event Minted(address dest, uint amount, address contractAddress);
 	constructor() public ERC20("SmartRetail Fidelity Token", "FDLT") {
 	}
@@ -14,5 +15,13 @@ contract FDLTToken is ERC20 {
 	function mintTokenTo(address _dest, uint _amount) public {
 		_mint(_dest, _amount);
 		emit Minted(_dest, _amount, address(this));
+=======
+	event Minted(address dest, uint amount);
+	constructor() public ERC20("SmartRetail Fidelity Token", "FDLT") {}
+
+	function mintTokenTo(address dest, uint amount) public {
+		_mint(dest, amount);
+		emit Minted(_dest, _amount);
+>>>>>>> update TokenManager for Event child
 	}
 }
