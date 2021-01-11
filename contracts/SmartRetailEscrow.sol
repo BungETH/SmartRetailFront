57c34d05244c263801692f43960ab05731dee2d9
Elie Vangu <vangu.elie@gmail.com>
2021-01-11 08:33:18 +0100

    change compiler version

--- contracts/SmartRetailEscrow.sol
+++ contracts/SmartRetailEscrow.sol
@@ -1,9 +1,5 @@
 // SPDX-License-Identifier: MIT
-<<<<<<< HEAD
-pragma solidity >=0.4.21 <0.7.5;
-=======
 pragma solidity 0.6.12;
->>>>>>> 41fe833db711b4a69b1a10b2a62f998ffaaf35a4
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/math/SafeMath.sol";
