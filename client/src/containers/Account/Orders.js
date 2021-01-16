4ca76e91b4fb7a929e85194b248080e5cc5dc5aa
Elie Vangu <vangu.elie@gmail.com>
2021-01-16 11:47:54 +0100

    merging preparation

--- client/src/containers/Account/Orders.js
+++ client/src/containers/Account/Orders.js
@@ -1,16 +1,23 @@
 import { connect } from 'react-redux';
 import { sendConfirmationDelivery } from '../../actions/escrow';
-import Orders from '../../components/Account/Orders.jsx';
+import { deleteOrder, fetchOrders } from '../../actions/orders';
+import Orders from '../../components/Account/Orders';
 
 const mapStateToProps = (state) => ({
-  orders: state.escrow.userOrders,
+  orders: state.escrow.orders,
   status: state.escrow.status,
 });
 
 const mapDispatchToProps = (dispatch) => ({
 
-  sendConfirmationDelivery: (orderId) => {
-    dispatch(sendConfirmationDelivery(orderId));
+  sendConfirmationDelivery: (referenceId, orderId) => {
+    dispatch(sendConfirmationDelivery(referenceId, orderId));
+  },
+  deleteOrder: ( orderId) => {
+    dispatch(deleteOrder(orderId));
+  },
+  fetchOrders: () => {
+    dispatch(fetchOrders());
   },
 });
 
